e66326e2a300349780a51b171a82ae3ed058a1e9
EmanAyman256 <emanayman256@gmail.com>
2025-01-06 10:15:12 +0200

    Testing-Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://sd-dev.c-878bd9e.kyma.ondemand.com',
-  apiUrl: 'https://sd-cf.cfapps.us10-001.hana.ondemand.com',
-  authUrl: "https://asarh4ioh.accounts.cloud.sap/oauth2/token",
-  clientID: "d5d5c915-b649-4ecb-9e15-30118433c22f",
-  clientSecret: "0v4281MqKGk:7Bcd[6UtgwevUPBXPGGYy"
+  // apiUrl: 'https://sd-cf.cfapps.us10-001.hana.ondemand.com',
+  // authUrl: "https://asarh4ioh.accounts.cloud.sap/oauth2/token",
+  //   clientID: "1830c61b-9601-4c91-a0c5-901cdbf0c2e4",
+  // clientSecret: "mGV7xPbl[MV51HG-lVeHQV2=LkTg=@"
+  //!Customer Credentials 
+  clientID: "9441fbe2-445a-46e2-9531-1ae48d35a5db",
+  clientSecret: "gWSnA-VU-rhaWe48NthVEyLlpNOl4RFXBhA"
 };
